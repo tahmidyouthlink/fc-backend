9f1685c22d167bdcc43e8bc40c66dedb4465f209
tahmidyouthlink <t.rahman@youthlink.tech>
2025-07-24 11:27:04 +0600

    change email user

--- utils/email/transportViaMailGun.js
+++ utils/email/transportViaMailGun.js
@@ -1,10 +1,10 @@
 const nodemailer = require("nodemailer");
 
 const transportViaMailGun = nodemailer.createTransport({
-  host: "smtp.mailgun.org",
+  host: process.env.EMAIL_HOST,
   port: 587,
   auth: {
-    user: process.env.SUPPORT_EMAIL,
+    user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS, // From Mailgun dashboard
   },
 });
