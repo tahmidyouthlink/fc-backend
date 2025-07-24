dc6a5df54f4147b95193b0746c5abd726c8f8971
tahmidyouthlink <t.rahman@youthlink.tech>
2025-07-24 11:52:54 +0600

    removed secure : true from transport

--- utils/email/transport.js
+++ utils/email/transport.js
@@ -3,7 +3,6 @@ const nodemailer = require("nodemailer");
 const transport = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
   port: process.env.EMAIL_PORT, // Use 587 for TLS
-  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
